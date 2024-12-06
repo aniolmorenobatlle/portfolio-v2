d44554ce2a84b0580f5bc3b56161d401847c26ae
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-12-06 14:24:14 +0100

    Header + Projects

---
+++ src/components/Header/MainHeader.vue
@@ -0,0 +1,71 @@
+<script setup></script>
+
+<template>
+  <div
+    class="mb-16 sm:mb-0 text-center scroll-mt-[100rem] particles-section pt-28 pb-14 sm:pt-36 sm:pb-18 w-full px-4 bg-black"
+  >
+    <div class="flex items-center justify-center">
+      <div class="relative">
+        <div style="opacity: 1; transform: none">
+          <img
+            src="/src/assets/img/amoreno.jpg"
+            alt="Aniol portrait"
+            fetchpriority="high"
+            width="192"
+            height="192"
+            class="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            style="color: transparent"
+          />
+        </div>
+        <span class="absolute bottom-0 right-0 text-4xl" style="opacity: 1; transform: none"
+          >👋</span
+        >
+      </div>
+    </div>
+
+    <h1
+      class="mb-10 mt-4 px-0 sm:px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl text-white max-w-[50rem] mx-auto"
+      style="opacity: 1; transform: none"
+    >
+      Hello, I'm Aniol. I'm a web and software developer. I'm currently working with Java, Laravel
+      and Vue.js.
+    </h1>
+
+    <div
+      class="flex flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+      style="opacity: 1; transform: none"
+    >
+      <a
+        class="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack text-gray-950"
+        href="https://github.com/aniolmorenobatlle"
+        target="_blank"
+      >
+        GitHub
+
+        <img
+          src="/src/components/icons/github.png"
+          alt="GitHub logo"
+          width="24"
+          height="24"
+          class="opacity-70"
+        />
+      </a>
+      <a
+        class="group bg-gray-950 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition border-2 border-white border-opacity-40"
+        href="https://www.linkedin.com/in/aniol-moreno-batlle/"
+        target="_blank"
+      >
+        <span class="opacity-70">LinkedIn</span>
+        <img
+          src="/src/components/icons/linkedin.png"
+          alt="LinkedIn logo"
+          width="24"
+          height="24"
+          class="opacity-70"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header/MainNav.vue
@@ -0,0 +1,82 @@
+<script setup></script>
+
+<template>
+  <div
+    class="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+    style="opacity: 1; transform: translateX(-50%) translateY(0px) translateZ(0px)"
+  ></div>
+  <nav
+    class="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0"
+  >
+    <ul
+      class="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5"
+    >
+      <li
+        class="h-3/4 flex items-center justify-center relative"
+        style="opacity: 1; transform: none"
+      >
+        <a
+          class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300 text-gray-950"
+          href="#home"
+        >
+          Home
+          <span
+            class="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+            style="transform: none; transform-origin: 50% 50% 0px"
+          >
+          </span>
+        </a>
+      </li>
+      <li
+        class="h-3/4 flex items-center justify-center relative"
+        style="opacity: 1; transform: none"
+      >
+        <a
+          class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300"
+          href="#projects"
+          >Projects</a
+        >
+      </li>
+      <li
+        class="h-3/4 flex items-center justify-center relative"
+        style="opacity: 1; transform: none"
+      >
+        <a
+          class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300"
+          href="#skills"
+          >Skills</a
+        >
+      </li>
+      <li
+        class="h-3/4 flex items-center justify-center relative"
+        style="opacity: 1; transform: none"
+      >
+        <a
+          class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300"
+          href="#experience"
+          >Experience</a
+        >
+      </li>
+      <li
+        class="h-3/4 flex items-center justify-center relative"
+        style="opacity: 1; transform: none"
+      >
+        <a
+          class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300"
+          href="#education"
+          >Education</a
+        >
+      </li>
+      <li
+        class="h-3/4 flex items-center justify-center relative"
+        style="opacity: 1; transform: none"
+      >
+        <a
+          class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300"
+          href="#contact"
+          >Contact</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
