36675467c1a8d4750557d81ee76455cb9d79f2c3
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2025-01-09 16:54:31 +0100

    Google Analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createI18n } from 'vue-i18n'
 import Cookies from 'js-cookie'
+import vueGtag from 'vue-gtag'
 
 import ca from './i18n/ca.json'
 import en from './i18n/en.json'
@@ -17,4 +18,11 @@ const i18n = createI18n({
   messages: { ca, en, es },
 })
 
-createApp(App).use(i18n).mount('#app')
+const app = createApp(App)
+
+// Configurar Google Analytics
+app.use(vueGtag, {
+  config: { id: 'G-4GLTRJSJVH' }
+})
+
+app.use(i18n).mount('#app')
